3c0e7c9d1c1cf8fe64558484b8aa5dd36d9d1500
Mike Kane <mi.kane@northeastern.edu>
2020-06-26 20:26:06 -0400

    Simplified main_LIB_simple.m by removing optimization into main_LIB_simple_opt.m.
    TODO, create main_flow_simple.m that removes optimization.

--- main_LIB_simple.m
+++ main_LIB_simple.m
@@ -1,6 +1,11 @@
-%MAIN_LIB_SIMPLE Optimize a simple island
+%MAIN_LIB_SIMPLE Simulate a simple island
 %   Size a single LIB and generator to meet demand using a simple charge
 %   law of charge when supply>demand, and discharge otherwise.
+
+%% Parameters
+
+% Generator
+gen_rated_power = 10^4; % kW
 
 %% Make island
 
@@ -13,17 +18,10 @@ fmincon_options = optimoptions(@fmincon, ...
     'Display','iter', ...
     'PlotFcn', {@optimplotfval,@optimplotstepsize});
 
-[gen_rated_power,fval,exitflag,output,population,scores] = fmincon( ...
-    @(x)(cost_fun(sys,x)), ...
-    0, ... nvars
-    [],[],[],[], ... A, b, Aeq, beq
-    sys.gens{1}.opt.lb, sys.gens{1}.opt.ub, ... lb, ub
-    [], ... nonlcon
-    fmincon_options);
+cost_fun(sys, gen_rated_power)
 
 [LCOE, LCOE_parts, LCOE_parts_names] = sys.LCOE(true);
 sys.plot(sprintf('LCOE: %.1f %s/kWh', LCOE*100,  char(0162)))
-
 
 function cost = cost_fun(sys, gen_rated_power)
     sys.gens{1}.rated_power = gen_rated_power;
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Tidal Microgrids for Small Island
+
+# Key files
+
+  * `main_LIB_simple` - Simulates the Lithium-ion battery with simple controller
+	
+  * `main_flow_simple` - Simulates the Flow battery
