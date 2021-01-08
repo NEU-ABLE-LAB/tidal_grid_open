f136f976a6663e03962e7d4f85562c6659014cc6
Kris Govertsen <govertsen.k@northeastern.edu>
2021-01-08 16:42:33 -0700

    Created modifyFigs
    this file takes each outputed figure from the figures folder in ondedrive and modifies its dimensions and font sizes, then saves a pdf and a png.

---
+++ modfiyFigs.m
@@ -0,0 +1,53 @@
+% This file retrieves the folder path for the S_Tidal figures.
+% The figures are loaded, edited, and saved as the final figures for
+% outputs as .fig and .pdf files
+% These finalized files must still be edited in illustrator/inkscape.
+%% Folder Path
+% Ask user for folder path
+prompt = 'Copy and paste the "figures" folder path for the S_Tidal project on your device\nExample: C:\\Users....\\figures\n';
+
+% Save user input
+folderPath = input(prompt,'s');
+
+% Change folderpath slashes from \ to / 
+folderPath = strrep(folderPath,'\','/');
+
+% Retrieve .fig file names from folder
+info.figNames = strip(erase(string(ls(strcat(folderPath,'/*.fig'))),'.fig'));
+
+% Specify the X and Y Dimensions for each figure in inches 
+info.XDim = [3.25;3.25;3.25;3.25;3.25;3.25;3.25;3.25];
+info.YDim = [3.25;3.25;3.25;3.25;3.25;3.25;3.25;3.25];
+
+%% Finalize Figures
+% Iterate through each file 
+for iFile = 1:length(info.figNames);
+    
+    % Retrieve File Name
+    figName = info.figNames{iFile};
+    
+    % Open figure
+    a = openfig(strcat(folderPath,'/',figName,'.fig'));
+    
+    % Open figure in a new window (NOT docked)
+    set(a, 'windowstyle','normal')
+    
+    % Set font size to 8 
+    set(gca,'FontSize',8)
+    
+    % Set Units of Figure to inches
+    a.Units = 'inches';
+        
+    % Resize image based on specified dimensions
+    a.Position = [1,1,info.XDim(iFile),info.YDim(iFile)];
+    
+    % Save png with minimal white space
+    exportgraphics(a,strcat(folderPath,'/Finalized/',figName,'.png'),'Resolution',300)
+    
+    % Save pdf with minimal white space
+    exportgraphics(a,strcat(folderPath,'/Finalized/',figName,'.pdf'),'Resolution',300)
+   
+    % Close figure
+    clf
+end
+
